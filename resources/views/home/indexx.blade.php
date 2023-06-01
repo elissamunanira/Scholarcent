4f47574c0d8c69c3fde7bdc523cb2d23a8a10051
elissamunanira <munanira64@gmail.com>
2023-06-01 10:52:28 +0200

    posts

--- resources/views/home/indexx.blade.php
+++ resources/views/home/indexx.blade.php
@@ -142,16 +142,6 @@
 
 			<div class="row align-items-stretch retro-layout-alt">
 
-				{{-- <div class="col-md-5 order-md-2">
-					<a href="single.html" class="hentry img-1 h-100 gradient">
-						<div class="featured-img" style="background-image: url('/home/images/img_2_vertical.jpg');"></div>
-						<div class="text">
-							<span>February 12, 2019</span>
-							<h2>Meta unveils fees on metaverse sales</h2>
-						</div>
-					</a>
-				</div> --}}
-
 				@foreach($jobPosts as $post)
 
 				<div class="col-md-6">
@@ -163,23 +153,6 @@
 							<h2>{{$post->title}}</h2>
 						</div>
 					</a>
-
-					{{-- <div class="two-col d-block d-md-flex justify-content-between">
-						<a href="single.html" class="hentry v-height img-2 gradient">
-							<div class="featured-img" style="background-image: url('/home/images/img_2_sq.jpg');"></div>
-							<div class="text text-sm">
-								<span>February 12, 2019</span>
-								<h2>Don’t assume your user data in the cloud is safe</h2>
-							</div>
-						</a>
-						<a href="single.html" class="hentry v-height img-2 ms-auto float-end gradient">
-							<div class="featured-img" style="background-image: url('/home/images/img_3_sq.jpg');"></div>
-							<div class="text text-sm">
-								<span>February 12, 2019</span>
-								<h2>Startup vs corporate: What job suits you best?</h2>
-							</div>
-						</a>
-					</div>   --}}
 
 				</div>
 				@endforeach
