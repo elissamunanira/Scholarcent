ad9515ed93582fe76a90ec41a1f248779288be13
elissamunanira <munanira64@gmail.com>
2023-06-03 15:23:40 +0200

    blog

--- resources/views/home/blogs.blade.php
+++ resources/views/home/blogs.blade.php
@@ -1,41 +1,6 @@
  @extends('layouts.apps')
  @section('content')
- <section class="blog-page">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 left-side">
-                <h2>Recent Blog Posts</h2>
-                  <div class="post">
-                    <div class="row">
-                        @foreach ($posts as $post)
-                        <div class="col-lg-6 col-md-12 col-12">
-                            <div class="col1">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <figure class="figure">
 
-                                            <a href="/posts/{{$post->id}}"> <img style="width :100%"src="/storage/images/{{$post->cover_image}}"></a>
-
-
-                                            {{-- <img src="images/banner-image-1.jpg" class="figure-img img-fluid" alt="A generic square placeholder image with rounded corners in a figure."> --}}
-                                        </figure>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <h4><a href="/posts/{{$post->id}}">{{$post->title}}</a></h4>
-                                        <div class="btn-group">
-                                            <a href="/posts/{{$post->id}}" class="btn btn-danger">Read more...</a> 
-                                        </div>
-                                        <p>Posted on {{$post->created_at}}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-
-            </div>
-        </div>
 <div class="section search-result-wrap">
     <div class="container">
       
@@ -48,17 +13,15 @@
               <span class="date">{{$post->created_at}} &bullet; <a href="#">{{$post->branch_name}}</a></span>
               <h2><a href="single.html">{{$post->title}}</a></h2>
               <p>{{$post->body}}</p>
-              <p><a href="single.html" class="btn btn-sm btn-outline-primary">Read More</a></p>
+              <p><a href="/posts/{{$post->id}}" class="btn btn-sm btn-outline-primary">Read More</a></p>
             </div>
           </div>
         </div>
       </div>
     </div>
-</div>
 
         <div class="row bt"> 
             {{$posts->links()}} 
         </div>
     </div>
-</section>
 @endsection
