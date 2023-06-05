601bf963057102be7a45a06371c8a1fc543ed35f
elissamunanira <munanira64@gmail.com>
2023-06-05 21:25:25 +0200

    blog

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -65,28 +65,18 @@
 
 							<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
 								<li class="active"><a href="/">Home</a></li>
-								{{-- <li class="has-children">
+								<li class="has-children">
 									<a href="category.html">Scholarship</a>
-									<ul class="dropdown">
-										<li><a href="search-result.html">Search Result</a></li>
-										<li><a href="blog.html">Blog</a></li>
-										<li><a href="single.html">Blog Single</a></li>
-										<li><a href="category.html">Category</a></li>
-										<li><a href="about.html">About</a></li>
-										<li><a href="contact.html">Contact Us</a></li>
-										<li><a href="#">Menu One</a></li>
-										<li><a href="#">Menu Two</a></li>
-										<li class="has-children">
-											<a href="#">Dropdown</a>
-											<ul class="dropdown">
-												<li><a href="#">Sub Menu One</a></li>
-												<li><a href="#">Sub Menu Two</a></li>
-												<li><a href="#">Sub Menu Three</a></li>
-											</ul>
-										</li>
-									</ul>
-								</li> --}}
-								<li><a href="/branches">Scholarship</a></li>
+									<ul class="dropdown-menu">
+                            <li>
+                                @foreach ($branches as $branch)
+                                <a href="{{url('/blog',$branch->id)}}">
+                                 {{ $branch->branch_name }}
+                                @endforeach
+                            </li> 
+                        </ul>
+								</li>
+								{{-- <li><a href="/branches">Scholarship</a></li> --}}
 								<li><a href="category.html">Internship</a></li>
 								<li><a href="category.html">Jobs</a></li>
 								<li><a href="category.html">Courses</a></li>
