c5ed373084f8c385eb419caaf5542e65ae06aec9
elissamunanira <munanira64@gmail.com>
2023-06-26 17:48:25 +0200

    social media

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -23,7 +23,7 @@
 	<link rel="stylesheet" href="/fonts/icomoon/style.css">
 	<link rel="stylesheet" href="/fonts/flaticon/font/flaticon.css">
 
-	<link rel="stylesheet" href="/https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
 	<link rel="stylesheet" href="/home/css/tiny-slider.css">
 	<link rel="stylesheet" href="/home/css/aos.css">
