c807862057a2c57c9200da1a7c28fba55987471f
elissamunanira <munanira64@gmail.com>
2023-06-30 09:38:11 +0200

    ckeditor

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -331,4 +331,25 @@ class PostsController extends Controller
         }
     }
 
+    public function uploadMedia(Request $request)
+{
+    if ($request->hasFile('upload')) {
+        $originName = $request->file('upload')->getClientOriginalName();
+        $fileName = pathinfo($originName, PATHINFO_FILENAME);
+        $extension = $request->file('upload')->getClientOriginalExtension();
+        $fileName = $fileName . '_' . time() . '.' . $extension;
+        $path = storage_path('app/public/tmp/uploads');
+        $fileName = $request->file('upload')->move($path, $fileName)->getFilename();
+
+        $CKEditorFuncNum = $request->input('CKEditorFuncNum');
+        $url = asset('storage/tmp/uploads/' . $fileName);
+        $msg = 'Image uploaded successfully';
+        $response = "<script>window.parent.CKEDITOR.tools.callFunction($CKEditorFuncNum, '$url', '$msg')</script>";
+
+        @header('Content-type: text/html; charset=utf-8');
+        echo $response;
+    }
+    return false;
+}
+
 }
