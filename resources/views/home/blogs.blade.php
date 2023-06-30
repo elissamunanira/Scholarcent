04888c4a6eb6275b01796e3b9fa74089facc9825
elissamunanira <munanira64@gmail.com>
2023-06-30 16:46:27 +0200

    blog

--- resources/views/home/blogs.blade.php
+++ resources/views/home/blogs.blade.php
@@ -4,7 +4,7 @@
 <div class="section search-result-wrap">
     <div class="container">
       
-      <div class="row">
+      {{-- <div class="row">
             @foreach($posts as $post)
 				<div class="col-lg-4 mb-4">
 					<div class="post-entry-alt">
@@ -20,7 +20,23 @@
             @endforeach
         </div>
       </div>
-    </div>
+      </div> --}}
+
+      <div class="row posts-entry">
+            @foreach($posts as $post)
+        <div class="col-lg-12">
+          <div class="blog-entry d-flex blog-entry-search-item">
+           <a href="/posts/{{$post->id}}" class="img-link"><img style="width :100%"src="/storage/images/{{$post->cover_image}}" alt="Image" class="img-fluid"></a>
+            <div>
+              <span class="date">{{$post->created_at}} &bullet; <a href="#">{{$post->branch_name}}</span>
+              <h2><a href="/posts/{{ $post->id }}">{{$post->title}}</a></h2>
+              <p><a href="/posts/{{$post->id}}" class="btn btn-sm btn-outline-primary">Read More</a></p>
+            </div>
+          </div>
+        </div>
+            @endforeach
+      </div>
+
     <div class="row bt"> 
         {{$posts->links()}} 
     </div>
