563f027309f52cdd10aa8a5d01db5060c21a49e9
elissamunanira <munanira64@gmail.com>
2023-07-09 17:57:37 +0200

    debug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -80,8 +80,6 @@
     <div class="content-wrapper">
         @include('inc.messages')
         @yield('content')
-         <a href='https://acadooghostwriter.com/'>Ghostwriter Acadoo</a> <script type='text/javascript' src='https://www.freevisitorcounters.com/auth.php?id=2501ab976c485734fd0f43599f9717d36397860b'></script>
-<script type="text/javascript" src="https://freevisitorcounters.com/en/home/counter/1052326/t/1"></script>
     </div>
     <!-- /.content-wrapper -->
     
@@ -121,7 +119,7 @@
         <div class="float-right d-none d-sm-inline">
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2023 Scholarcent </strong> All rights reserved.
+        <center><strong>Copyright &copy; 2023 Scholarcent </strong> All rights reserved.</center>
     </footer>
 </div>
 <!-- ./wrapper -->
