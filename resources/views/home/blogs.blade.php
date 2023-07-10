8228f329b0f3662fdc4449077d4e73ca0cc8695d
elissamunanira <munanira64@gmail.com>
2023-07-10 17:26:38 +0200

    blogs

--- resources/views/home/blogs.blade.php
+++ resources/views/home/blogs.blade.php
@@ -24,8 +24,8 @@
 
       <div class="row posts-entry">
             @foreach($posts as $post)
-        <div class="col-md-6 col-lg-3">
-          <div class="blog-entry">
+        <div class="col-lg-6">
+          <div class="blog-entry d-flex blog-entry-search-item">
            <a href="/posts/{{$post->id}}" class="img-link"><img style="width :100%"src="/storage/images/{{$post->cover_image}}" alt="Image" class="img-fluid"></a>
             <div>
               <span class="date">{{$post->created_at}} &bullet; <a href="#">{{$post->branch_name}}</span>
