2a673dd05e881c1f2e324429de683438b01f2484
elissamunanira <munanira64@gmail.com>
2023-07-12 15:18:19 +0200

    post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -183,9 +183,8 @@ class PostsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id, $title)
+    public function update(Request $request, $title, $id)
     {
-
         $this -> validate($request , [
             'title' => 'required',
             'body' => 'required'
@@ -238,7 +237,7 @@ class PostsController extends Controller
         if ($branch_details) {
             $branch_name = $branch_details->branch_name;
 
-            $post = Post::find($title);
+            $post = Post::where('title', $title)->firstOrFail();
 
             $post->branch_name = $branch_name;
             $post->title = $request->input('title');
@@ -257,7 +256,7 @@ class PostsController extends Controller
             }
 
             // If $branch_details is null, continue with updating the other post details
-            $post = Post::find($title);
+            $post = Post::where('title', $title)->firstOrFail();
 
             $post->title = $request->input('title');
             $post->body = $request->input('body');
